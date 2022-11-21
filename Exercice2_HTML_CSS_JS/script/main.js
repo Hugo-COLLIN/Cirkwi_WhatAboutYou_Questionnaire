d97fbc8e3a096c2f703310390fb71d1c583235a9
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-22 00:06:48 +0100

    cleaning code

--- Exercice2_HTML_CSS_JS/script/main.js
+++ Exercice2_HTML_CSS_JS/script/main.js
@@ -1,28 +1,5 @@
-/*$(function ()
-{
-    const btn = $(".menu-item > button");
-    btn.click(function () {
-       //$(".menu-content").addClass(".menu-content-deployed");
-        alert("Ca a cliqué")
-    })
-
-
-});*/
-/*
-$(function() {
-    $('button').click(function() {
-        $(this).next().slideToggle('500').css('');
-    });
-    $('.enter').each().click(function() {
-        $('.para').addClass('.active');
-    });
-
-});*/
-
 $(function($) {
     $('button').click(function() {
-        //$(this).next().slideToggle('500').css('');
         $(this).toggleClass('menu-button-deployed').next().toggleClass('menu-content-deployed');
     });
-
 });
